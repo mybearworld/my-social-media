ca68c7bff92c4e943ae06267aa5f9638d5d31ee3
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-31 17:02:31 +0200

    Styling

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 
 const createLetter = () => {
   const element = document.createElement("div");
+  element.className = "base-character";
   const letter = document.createElement("span");
-  let codepoint = 65;
-  letter.textContent = "A";
+  letter.className = "character";
+  let codepoint = 42;
+  letter.textContent = "*";
   element.append(letter);
   const down = document.createElement("button");
+  down.className = "down";
   down.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>`; // from lucide icons
   element.append(down);
   down.addEventListener("click", () => {
